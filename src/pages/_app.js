5cad7958135b3d6debb13c03844c5994bb68e085
kiukmaster <124538821+kiukmaster@users.noreply.github.com>
2023-06-18 20:21:52 +0900

    metadata

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,10 +8,10 @@ export default function App({Component, pageProps}) {
             <Layout />
             <Head>
                 <meta name="description" content="마인크래프트 렌독서버 도우미 사이트입니다." />
-                <meta name="keywords" content="마인크래프트, 마크, 렌독서버, 렌독, 랜독, 랜독서버, 마크서버, 마크렌독, Rendog, Minecraft Rendog Server" />
+                <meta name="keywords" content="마인크래프트, 마크, 렌독서버 도우미, 렌독 서버 도우미, 렌독서버도우미, 렌독도우미, 렌독 도우미, 랜독도우미, 랜독 도우미, 랜독서버도우미, 랜독 서버 도우미, 랜독서버 도우미, 렌독서버, 렌독, 랜독, 랜독서버, 마크서버, 마크렌독, Rendog, Minecraft Rendog Server" />
                 <link rel="icon" href="./mainimg/Rendog.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>렌독서버 도우미 - RendogHelper</title>
+                <title>렌독서버 도우미</title>
             </Head>
             <Component {...pageProps} />
         </>
