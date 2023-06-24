2795aa1faf6bbaf9295b0c33ef6ec4520036baaa
kiukmaster <124538821+kiukmaster@users.noreply.github.com>
2023-06-24 15:05:08 +0900

    ga real x 3 set

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }) {
                     __html: ` window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '[G-TKHH72GXBD]', { page_path: window.location.pathname });
+                    gtag('config', 'G-TKHH72GXBD', { page_path: window.location.pathname });
                     `,
                 }} />
 
